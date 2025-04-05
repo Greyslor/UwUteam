1fcafcdde9cc8d949d1a30a6ecc2e2b1a75550f6
Greyslor <cdmx2510@amerike.edu.mx>
2025-04-04 18:53:06 -0600

    Prueba Websocket

--- 03_node/03_websocket/server.js
+++ 03_node/03_websocket/server.js
@@ -1,34 +1,75 @@
 const WebSocket = require('ws');
-
 const clients = [];
 
-const wss = new WebSocket.Server({ port: 8080 }, ()=>{
+class Cliente{
+	constructor()
+	{
+		this._username="No name";
+	}
+
+	set username( user )
+	{
+		this._username = user;
+	}
+
+	get username ()
+	{
+		return this._username;
+	}
+}
+
+const wss = new WebSocket.Server({ port: 8080 },()=>{
     console.log('Server Started');
 });
 
 wss.on('connection', function connection(ws) {
-    console.log('Se conectó un cliente...');
-    clients.push(ws);
+	
+	console.log('New connenction');
+	clients.push(ws); // Agregar la conexión (cliente) a la lista
 
+	let cliente = new Cliente ();
+	
     ws.on('open', (data) => {
-        console.log('New Connection');
-    });
+		console.log('Now Open');
+	});
 
-wss.on('message', (data) => {
-    
-        console.log('Data received: %s', data);
-        
-        //ws.send("The server renponse: " + data);
+	ws.on('message', (data) => {
+		console.log('Data received: %s',data);
+		
+		//ws.send("The server response: "+data); // Para mandar el mensaje al cliente que lo envió
 
-        clients.forEach(client => {
-            if(client.readyState === WebSocket.OPEN)
-            {
-                client.send(data);
-            }
-        });
-    });    
+		let info = data.toString().split('|');
+
+		switch (info[0])
+		{
+			case '200':
+				cliente.username = info[1];
+				ws.send("UserName upDated: "+cliente.username);
+				break;
+			
+				default:
+					// Mandar a todos los clientes conectados el mensaje con el username de quien lo envió
+					clients.forEach(client => {
+						if(client.readyState === WebSocket.OPEN)
+						{
+							client.send(cliente.username + " says: " + data); // si falla, cambiar a: `data.toString()`
+						}
+					});
+					break;
+		}
+	});
+
+	// Al cerrar la conexión, quitar de la lista de clientes
+	ws.on('close', () => { 
+		let index = clients.indexOf(ws);
+		if(index > -1)
+		{
+			clients.splice(index, 1);
+			ws.send("UserName disconnected: "+cliente.username);
+		}
+	});
 });
 
 wss.on('listening',()=>{
-    console.log('listening on 8080');
+   console.log('Now listening on port 8080...');
 });
